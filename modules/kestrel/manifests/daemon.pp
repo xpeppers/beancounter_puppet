b0a354e31644854e888af0f923ef39dbd2dbdb07
JoeBew42 <joebew42@gmail.com>
2014-01-13 13:00:38 +0100

    Fixing daemon installation and kestrel service start

--- modules/kestrel/manifests/daemon.pp
+++ modules/kestrel/manifests/daemon.pp
@@ -1,25 +1,25 @@
 class daemon {
   download_file {
-  	['daemon-0.6.4.tar.gz']:
-  	site => 'http://libslack.org/daemon/download',
-  	cwd => '/tmp/other-repos',
-  	require => File['/tmp/other-repos'],
+    ['daemon-0.6.4.tar.gz']:
+    site => 'http://libslack.org/daemon/download',
+    cwd => '/tmp/other-repos',
+    require => File['/tmp/other-repos'],
     before => Exec['daemon'],
-  	unless => '/usr/bin/test -f /usr/local/bin/daemon',
-  } 
+    unless => '/usr/bin/test -f /usr/local/bin/daemon',
+  }
   exec {'daemon':
-  	command => '/bin/tar xzf daemon-0.6.4.tar.gz &&
-  	cd daemon-0.6.4 &&
-  	./config &&
-  	make PREFIX=/tmp/other-repos/daemon-0.6.4; ',
-  	cwd     => '/tmp/other-repos',  
+    command => '/bin/tar xzf daemon-0.6.4.tar.gz &&
+    cd daemon-0.6.4 &&
+    ./config &&
+    make PREFIX=/tmp/other-repos/daemon-0.6.4; ',
+    cwd     => '/tmp/other-repos',
     require => Exec['daemon-0.6.4.tar.gz'],
-    unless => '/usr/bin/test -f /usr/local/bin/daemon', 
-  } 
+    unless => '/usr/bin/test -f /usr/local/bin/daemon',
+  }
   exec {'daemon install':
-    command => '/usr/bin/make install PREFIX=/tmp/other-repos/daemon-0.6.4;',
-    cwd     => '/tmp/other-repos/daemon-0.6.4',  
+    command => '/usr/bin/make install',
+    cwd     => '/tmp/other-repos/daemon-0.6.4',
     require => Exec['daemon'],
-    unless => '/usr/bin/test -f /usr/local/bin/daemon', 
-  } 
+    unless => '/usr/bin/test -f /usr/local/bin/daemon',
+  }
 }
--- modules/kestrel/manifests/init.pp
+++ modules/kestrel/manifests/init.pp
@@ -2,29 +2,29 @@ class kestrel {
   require daemon
 
   download_file {
-  	['kestrel-2.4.1.zip']:
-  	site => 'http://robey.github.io/kestrel/download/',
-  	cwd => '/tmp/other-repos',
-  	require => [
-  		File['/tmp/other-repos'],
-  		File['/var/run/kestrel'],
-  		Package['unzip']
-  		],
-  	unless => "/usr/bin/test -x /etc/init.d/kestrel",
-  	before => Exec['kestrel']
-  } 
+    ['kestrel-2.4.1.zip']:
+    site => 'http://robey.github.io/kestrel/download/',
+    cwd => '/tmp/other-repos',
+    require => [
+        File['/tmp/other-repos'],
+        File['/var/run/kestrel'],
+        Package['unzip']
+        ],
+    unless => "/usr/bin/test -x /etc/init.d/kestrel",
+    before => Exec['kestrel']
+  }
   if $hardwaremodel == 'x86_64' {
     $JVM_NAME = 'java-6-openjdk-amd64'
   } else {
     $JVM_NAME = 'java-6-openjdk-i386'
   }
   exec {'kestrel':
-  	command => '/usr/bin/unzip /tmp/other-repos/kestrel-2.4.1.zip;
+    command => '/usr/bin/unzip /tmp/other-repos/kestrel-2.4.1.zip;
     /bin/cp -r /tmp/other-repos/kestrel-2.4.1 /usr; ',
-  	cwd => '/tmp/other-repos',
+    cwd => '/tmp/other-repos',
     unless => "/usr/bin/test -x /etc/init.d/kestrel",
-  	require =>  File['/var/log/kestrel'],    
-  } -> 
+    require =>  File['/var/log/kestrel'],
+  } ->
   file {'/etc/init.d/kestrel':
     ensure      => file,
     mode   => 755,
@@ -36,19 +36,19 @@ class kestrel {
     content     => template('kestrel/usr/kestrel-2.4.1/config/production.scala.erb'),
   } ->
   exec {'add kestrel service':
-  	command => '/sbin/chkconfig --add kestrel;
- 		/sbin/chkconfig kestrel on; '
+    command => '/sbin/chkconfig --add kestrel;
+        /sbin/chkconfig kestrel on; '
   }
 
   package { 'unzip':
-  	ensure => present
+    ensure => present
   }
 
   service { 'kestrel':
     enable    => true,
     ensure    => running,
     hasstatus => true,
-    require   => Exec['add kestrel service'],
+    subscribe => Exec['add kestrel service'],
   }
 
   file {'/var/log/kestrel':
