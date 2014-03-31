8807de9514f2d9cec479585453f0ccb187dd298d
Marco Trincardi <m@g.com>
2014-03-31 15:20:27 +0200

    adding configuration for beancounter stage

---
+++ stagebc/manifests/aws.pp
@@ -0,0 +1,94 @@
+#BEANCOUNTER main puppet
+
+file {'/etc/resolv.conf':
+  ensure      => present,
+  owner       => 'root',
+  group       => 'root',
+  mode        => 0644,
+  content     => "nameserver 8.8.8.8\nnameserver 8.8.4.4\n",
+  links       => follow,
+  before      => Exec['apt-get update'],
+}
+file {'/usr/local/beancounter':
+  ensure    => directory,
+  owner       => 'root',
+  group       => 'root',
+  mode        => 0775,
+}
+file { '/sbin/insserv':
+   ensure => 'link',
+   target => '/usr/lib/insserv/insserv',
+}
+user { 'vagrant':
+  ensure => present,
+  shell => '/bin/bash',
+  home => '/home/vagrant',
+  managehome => true,
+}
+group { 'tomcat7':
+  ensure => present
+}
+user { 'deploy':
+  ensure => present,
+  shell => "/bin/bash",
+  home => "/home/deploy",
+  managehome => true,
+  gid => '1000',
+  groups => ['root', 'tomcat7', 'admin']
+}
+ssh_authorized_key{ "deploy": 
+  user => "deploy",
+  ensure => present, 
+  type => "ssh-rsa", 
+  key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDiGqNDqOUorSKA8o5DpfZiP19DvSOdSRURVlcw3KO2mLwrrGQnHlw1d4GBqNXJy9NtWURG9cvkGWsF4KO/EYiCT8/Oo+lUbO0e5i+zECtKzoDPHqNsl3z/qgWyeldxyO2yDQCPbYzcliqhaYO2htk5tC4VHuuQyHl24GyVEHN/zOKlevraVyj0hTdfGtMv906E5LsGgNgoSfyn9zy3TGN/aIXirLU8oB+57IELTYxJAnPN3iWQZPTr7gIx5kZEoIyavRswytUSQ4b8m0dIZFBHuJrz0BPpdzkYBVMYaEIAjdOyIYBE6Wh2evUxcjab8FdbD28ONeHxT9Lth7b7Plnz", 
+  name => "deploy-public-key" 
+ } 
+
+exec {'apt-get update':
+  command => '/usr/bin/apt-get update --fix-missing',
+  before      => Package['chkconfig'],
+}
+
+package { 'chkconfig':
+  ensure => present,
+  before => Class['elasticsearch']  
+}
+class {'elasticsearch':
+  before      => [
+    Class['apache2'], 
+    Class['tomcat7']
+  ],
+}
+class {'apache2':
+  before 	  => Class['rvm'],
+}
+class { 'tomcat7':
+    jdk => 'openjdk-7-jdk',
+}
+class { 'kestrel':
+  before => Class['tomcat7'] 
+}
+class { 'redis':
+  before => Class['kestrel']
+}
+
+class { 'rvm': 
+  version => '1.20.12' 
+  } -> #and then
+file { '/home/vagrant/.rvm':
+   ensure => 'link',
+   target => '/usr/local/rvm',
+} -> #and then
+file { '/home/deploy/.rvm':
+   ensure => 'link',
+   target => '/usr/local/rvm',
+} -> #and then
+rvm_system_ruby {
+  'ruby-1.9.3-p429':
+    ensure => 'present',
+    default_use => true;
+}
+#rvm::system_user { vagrant: ; }
+
+
+
